50b9f58da111db430298b720463d3179fd87f261
ast21 <ast21@users.noreply.github.com>
2023-05-01 11:12:25 +0000

    Fix styling

--- src/Abstracts/Providers/RouteServiceProvider.php
+++ src/Abstracts/Providers/RouteServiceProvider.php
@@ -14,14 +14,14 @@ abstract class RouteServiceProvider extends LaravelRouteServiceProvider
      */
     public function boot(): void
     {
-//        $this->routes(function () {
-//            Route::middleware('api')
-//                ->prefix('api')
-//                ->group(base_path('routes/api.php'));
-//
-//            Route::middleware('web')
-//                ->group(base_path('routes/web.php'));
-//        });
+        //        $this->routes(function () {
+        //            Route::middleware('api')
+        //                ->prefix('api')
+        //                ->group(base_path('routes/api.php'));
+        //
+        //            Route::middleware('web')
+        //                ->group(base_path('routes/web.php'));
+        //        });
     }
 
     /**
