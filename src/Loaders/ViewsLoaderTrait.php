dc4321259c78abbaea83371649068f248d0fdf46
Anastas Mironov <ast@l80.ru>
2023-06-22 13:06:54 +0600

    refactor: remove unused code

--- src/Loaders/ViewsLoaderTrait.php
+++ src/Loaders/ViewsLoaderTrait.php
@@ -8,15 +8,13 @@ trait ViewsLoaderTrait
 {
     public function loadViewsFromContainers($containerPath): void
     {
-        $containerViewDirectory = $containerPath.'/UI/WEB/Views/';
-        $containerMailTemplatesDirectory = $containerPath.'/Mails/Templates/';
+        $containerViewDirectory = $containerPath . '/UI/WEB/Views/';
+        $containerMailTemplatesDirectory = $containerPath . '/Mails/Templates/';
 
         $containerName = basename($containerPath);
-        $pathParts = explode(DIRECTORY_SEPARATOR, $containerPath);
-        $sectionName = $pathParts[count($pathParts) - 2];
 
-        $this->loadViews($containerViewDirectory, $containerName, $sectionName);
-        $this->loadViews($containerMailTemplatesDirectory, $containerName, $sectionName);
+        $this->loadViews($containerViewDirectory, $containerName);
+        $this->loadViews($containerMailTemplatesDirectory, $containerName);
     }
 
     private function loadViews($directory, $containerName): void
