3e3c9b4f87f4084afe9de60220f138c5dbd3ac3b
Anastas Mironov <ast@l80.ru>
2023-08-18 12:54:08 +0600

    fix: correct FilamentResourceGenerator

--- src/Commands/FilamentResourceGenerator.php
+++ src/Commands/FilamentResourceGenerator.php
@@ -16,11 +16,23 @@ class FilamentResourceGenerator extends AbstractGeneratorCommand
 
     protected $folderInsideContainer = 'UI/Filament/Resources';
 
+    public function handle()
+    {
+        parent::handle();
+
+        $name = $this->qualifyClass($this->getNameInput());
+        $this->makeFilamentResourcePage($name, 'Create');
+        $this->makeFilamentResourcePage($name, 'Edit');
+        $this->makeFilamentResourcePage($name, 'List');
+    }
+
     protected function getVariables()
     {
         return [
-            '{{ modelName }}' => $this->argument('name'),
-            '{{ modelNamespace }}' => $this->getContainerNamespace() . '\\Models\\' . $this->argument('name'),
+            '{{ modelName }}' => $name = $this->argument('name'),
+            '{{ modelNamespace }}' => $this->getContainerNamespace() . '\\Models\\' . $name,
+            '{{ label }}' => $name,
+            '{{ pluralLabel }}' => Str::plural($name),
         ];
     }
 
@@ -29,24 +41,11 @@ class FilamentResourceGenerator extends AbstractGeneratorCommand
         return parent::getNameInput() . 'Resource';
     }
 
-    protected function buildClass($name)
-    {
-        $this->makeFilamentResourcePage($name, 'Create');
-        $this->makeFilamentResourcePage($name, 'Edit');
-        $this->makeFilamentResourcePage($name, 'List');
-
-        $stub = $this->files->get($this->getStub());
-
-        return $this->replaceVariables($stub)
-            ->replaceNamespace($stub, $name)
-            ->replaceClass($stub, $name);
-    }
-
     protected function makeFilamentResourcePage($name, $page)
     {
-        $stubPath = file_exists($path = base_path('stubs/porto/' . 'filament.resource.'.strtolower($page).'.stub'))
+        $stubPath = file_exists($path = base_path('stubs/porto/' . 'filament.resource.' . strtolower($page) . '.stub'))
             ? $path
-            : __DIR__ . '/stubs/' . 'filament.resource.'.strtolower($page).'.stub';
+            : __DIR__ . '/stubs/' . 'filament.resource.' . strtolower($page) . '.stub';
 
         $stub = $this->files->get($stubPath);
         $stub = $this->replaceVariables($stub)
@@ -54,9 +53,9 @@ class FilamentResourceGenerator extends AbstractGeneratorCommand
             ->replaceClass($stub, $name);
 
         $name = Str::replaceFirst($this->rootNamespace(), '', $name);
-        $pageFilepath = $this->laravel['path'].'/'.
-            str_replace('\\', '/', $name).'/Pages/'.
-            ucfirst($page).$this->argument('name').'.php';
+        $pageFilepath = $this->laravel['path'] . '/' .
+            str_replace('\\', '/', $name) . '/Pages/' .
+            ucfirst($page) . $this->argument('name') . '.php';
         $this->makeDirectory($pageFilepath);
 
         $this->files->put($pageFilepath, $this->sortImports($stub));
